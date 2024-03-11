767a82e63a980c8fedc0e702a531819b4f5106e6
felipegilisk <felipegilisk@gmail.com>
2024-03-11 02:00:28 -0300

    initial commit

--- simplemooc/core/views.py
+++ simplemooc/core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello World") 
+def index(request):
+    return render(request, 'index.html', {'usuario': 'NOME_USUARIO'})
+
+def contact(request):
+    return render(request, 'contact.html')
--- simplemooc/simplemooc/urls.py
+++ simplemooc/simplemooc/urls.py
@@ -19,5 +19,5 @@ from core.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls'))
+    path('', include(('core.urls', 'core'), namespace='core'))
 ]
